94e40389e28f5297eac666203d6e95a2943d7853
jorgangelov <jorgangelov@gmail.com>
2016-10-11 18:01:10 +0200

    Changed Orientation according to Quadcopter Code

--- Orientation/IMU.h
+++ Orientation/IMU.h
@@ -4,6 +4,8 @@
 #include <Arduino.h>
 #include "EEPROM.h"
 #include "Wire.h"
+#include "myMATH.h"
+
 #define TRANSFORMATION false
 
 void blink(int n);
@@ -37,27 +39,6 @@ struct tSensor
     float bias_mx;
     float bias_my;
     float bias_mz;
-};
-
-
-class cQuaternion
-{
-public:
-    cQuaternion(float q1=1,float q2=0,float q3=0,float q4=0)
-    {
-        values[0] = q1;
-        values[1] = q2;
-        values[2] = q3;
-        values[3] = q4;
-    }
-
-    cQuaternion operator+(cQuaternion Q);
-    cQuaternion operator*(cQuaternion Q);
-    cQuaternion operator*(float f);
-    float& operator()(int i);
-    cQuaternion conjugated();
-    void norm();
-    float values[4];
 };
 
 
