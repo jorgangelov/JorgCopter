657f83b4c27b5ae7ddc28185efeed337b80be617
jorgangelov <jorgangelov@gmail.com>
2016-10-17 07:24:21 +0200

    All Math Operations in myMATH.h

--- Quadcopter_Mini/IMU.h
+++ Quadcopter_Mini/IMU.h
@@ -4,6 +4,7 @@
 #include <Arduino.h>
 #include "EEPROM.h"
 #include "Wire.h"
+#include "myMATH.h"
 #define TRANSFORMATION false
 
 void blink(int n);
@@ -30,34 +31,6 @@ struct tSensor
     float bias_wz;
 };
 
-
-struct tEuler
-{
-    float phi;
-    float theta;
-    float psi;
-};
-
-
-class cQuaternion
-{
-public:
-    cQuaternion(float q1=1,float q2=0,float q3=0,float q4=0)
-    {
-        values[0] = q1;
-        values[1] = q2;
-        values[2] = q3;
-        values[3] = q4;
-    }
-
-    cQuaternion operator+(cQuaternion Q);
-    cQuaternion operator*(cQuaternion Q);
-    cQuaternion operator*(float f);
-    float& operator()(int i);
-    cQuaternion conjugated();
-    void norm();
-    float values[4];
-};
 
 
 class cImu
